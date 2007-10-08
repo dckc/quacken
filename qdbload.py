55b01b33c496aeddbd5b1948fbd4ce6087277e41
Dan Connolly http://www.w3.org/People/Connolly/ <none@none>
2007-10-08 14:01:39 -0500

    factor out CSV operations in preparation for a try with sqlite

--- qdbload.py
+++ qdbload.py
@@ -8,56 +8,70 @@ import csv
 def main(args):
     fnames = args[1:]
     txs = trxiter(fnames)
-    normalize(txs)
+    normalize(txs, CSVDB())
 
-def normalize(txs):
+def normalize(txs, db):
     # running ids for transactions, splits
     tid = 1
     sid = 1
 
-    txw = csv.writer(open("transactions.csv", "wb"))
-    splits = csv.writer(open("splits.csv", "wb"))
-    accounts = nametable("accounts")
-    accounts.open()
-    classes = nametable("classes")
-    classes.open()
+    txw = db.mktable("transactions")
+    splits = db.mktable("splits")
+    accounts = NameTable("accounts")
+    accounts.open(db)
+    classes = NameTable("classes")
+    classes.open(db)
 
     for trx in trxdetails(txs):
         tx = trx['trx']
-        txw.writerow((tid, accounts[tx['acct']],
-                      tx['date'],
-                      tx.get('payee', None),
-                      tx.get('num', None),
-                      tx.get('ty', None),
-                      tx.get('memo', None)
-                      ))
-
+        db.insert(txw,
+                  (tid, accounts[tx['acct']],
+                   tx['date'],
+                   tx.get('payee', None),
+                   tx.get('num', None),
+                   tx.get('ty', None),
+                   tx.get('memo', None)
+                   ))
+        
         for split in trx['splits']:
 
             # combine categories and transfer accounts
             a2 = split.get('acct', split.get('cat'))
             if 'class' in split:
                 cls = classes[split['class']]
+                if split['class'] == 'A]':
+                    raise ValueError, trx
             else:
                 cls = None
-            splits.writerow((sid, tid,
-                             accounts[a2],
-                             cls,
-                             split.get('clr', None),
-                             split.get('memo', None),
-                             split['subtot']))
+            db.insert(splits,
+                      (sid, tid,
+                       accounts[a2],
+                       cls,
+                       split.get('clr', None),
+                       split.get('memo', None),
+                       split['subtot']))
             sid += 1
         tid += 1
 
 
-class nametable(object, DictMixin):
+
+class CSVDB(object):
+    def mktable(self, name):
+        return csv.writer(open(name + ".csv", "wb"))
+
+    def insert(self, t, row):
+        t.writerow(row)
+
+
+class NameTable(object, DictMixin):
     def __init__(self, name):
         self._name = name
         self._d = {}
         self._i = 1
 
-    def open(self):
-        self._wr = csv.writer(open(self._name + ".csv", "wb"))
+    def open(self, db):
+        self._db = db
+        self._t = db.mktable(self._name)
 
     def __getitem__(self, k):
         d = self._d
@@ -66,7 +80,7 @@ class nametable(object, DictMixin):
         except KeyError:
             v = self._i
             d[k] = v
-            self._wr.writerow((v, k))
+            self._db.insert(self._t, (v, k))
             self._i += 1
             return v
 
