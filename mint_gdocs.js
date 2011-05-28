8f9807bb178e263362aff21bd2f81916c6124f6c
Dan Connolly <http://www.madmode.com/>
2011-05-28 15:18:24 -0500

    toward Mint/gdocs bookmark sync: bookmarklet infrastructure

---
+++ mint_gdocs.js
@@ -0,0 +1,24 @@
+alert("got this far!");
+
+
+/*
+Mint.PlanningData.getJson(true, apr);
+Mint.CategorySearch.getJson();
+
+apr = new Date(2011, 03, 01);
+// Fri Apr 01 2011 00:00:00 GMT-0500 (CDT)
+Object
+{
+bu: Array[6]
+0: Object
+amt: 1522.5
+bgt: 1250
+cat: 1050996
+ex: false
+id: 25616372
+pid: 30
+st: 1
+type: 0
+}
+
+*/
