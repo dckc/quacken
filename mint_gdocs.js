b865d92306dcb0a3b1e5cf03f10129eb1976db3f
Dan Connolly <http://www.madmode.com/>
2011-05-28 15:48:24 -0500

    bookmarklet enumerates top-level budget categories in a pop-up window

--- mint_gdocs.js
+++ mint_gdocs.js
@@ -4,10 +4,16 @@ function make_budget_window() {
     ugly_but_reportedly_necessary_global = window.open('', 'budget_window',
 						       'height=400, width=600');
     var d = ugly_but_reportedly_necessary_global.document;
-    d.write("hello world!");
+    d.write("hello world! bis");
+    var categories = Mint.CategorySearch.getJson();
+    d.write("<p>categories:" + categories.length + "</p>");
+    d.write("<ul>\n");
+    for (var i=0; i < categories.length; i++) {
+	d.write("<li>" + categories[i].value + "</li>\n");
+    }
+    d.write("</ul>\n");
 }
 
-//alert("got this far!");
 make_budget_window();
 
 /*
