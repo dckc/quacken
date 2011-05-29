980a4256195d775a8fc80124bea095d720e18981
Dan Connolly <http://www.madmode.com/>
2011-05-29 00:20:57 -0500

    use appendChild etc. rather than document.write  - tried integrating with YUI from Mint    - Mint includes yui3.js but seems to do most of its work with YUI 2

--- mint_gdocs.js
+++ mint_gdocs.js
@@ -1,17 +1,25 @@
 
-function make_budget_window() {
+function make_budget_window(){
+    var categories = Mint.CategorySearch.getJson();
+
     //http://www.quirksmode.org/js/popup.html
     ugly_but_reportedly_necessary_global = window.open('', 'budget_window',
 						       'height=400, width=600');
     var d = ugly_but_reportedly_necessary_global.document;
-    d.write("hello world! bis");
-    var categories = Mint.CategorySearch.getJson();
-    d.write("<p>categories:" + categories.length + "</p>");
-    d.write("<ul>\n");
-    for (var i=0; i < categories.length; i++) {
-	d.write("<li>" + categories[i].value + "</li>\n");
+    var b = d.body;
+
+    var mk = function (p, n) {
+	var e = d.createElement(n);
+	p.appendChild(e);
+	return e;
     }
-    d.write("</ul>\n");
+
+    mk(b, 'h1').textContent = 'Budget';
+    var t = mk(b, 'table');
+    mk(mk(t, 'tr'), 'th').textContent = 'Category';
+    categories.forEach(function(cat) {
+	mk(mk(t, 'tr'), 'td').textContent = cat.value;
+    });
 }
 
 make_budget_window();
