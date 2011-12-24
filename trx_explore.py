8525125a4df95d28350283adb5321674449024c1
dconnolly@186.43.168.192.in-addr.arpa <dconnolly@186.43.168.192.in-addr.arpa>
2011-12-24 14:43:19 -0600

    exploring transactions and labels

--- trx_explore.py
+++ trx_explore.py
@@ -1,9 +1,24 @@
 import json
 import pprint
 
+from bunch import Bunch
+
 def explore(fp):
-    x = json.load(fp)
-    pprint.pprint(x)
+    data = json.load(fp)
+
+    trxs = [Bunch.fromDict(o) for o in data]
+    trxs.sort(key=lambda(tx): int(tx.id))
+    pprint.pprint([(tx.id, tx.date,
+                    (int(tx.amount.replace('$', '').\
+                         replace(',', '').replace('.', '')) * (
+                             -1 if tx.isDebit else 1), 100),
+                    tx.amount, tx.isDebit) for tx in trxs])
+
+    print "Labels:"
+    pprint.pprint([(tx.id, l['id'], l['name'])
+                   for tx in trxs
+                   for l in tx.labels])
+
 
 def main(argv):
     fn = argv[1]
