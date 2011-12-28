4863aa974437ec9249795a4b4f2c3ef8eabec229
Dan Connolly <dconnolly@kumc.edu>
2011-12-28 17:46:10 -0600

    figured out how to import mint accounts into gnucash DB

--- mint_match.sql
+++ mint_match.sql
@@ -1,8 +1,67 @@
 SELECT * FROM `gnucash`.`mintexport`;
 
-use gnucash;
+use dm93finance;
 
-create index trx_sig on mintexport (date, original_description(300), transaction_type, amount);
+select ma.* from
+(select distinct account_name from mintexport) ma
+left join accounts ga on ga.name = ma.account_name
+where ga.name is null
+;
+select * from mintexport where account_name='Cash';
+
+-- guid column is only 32 chars long
+select length(replace(uuid(), '-', ''));
+
+insert into accounts
+select replace(uuid(), '-', '') as guid
+     , mc.category as name
+     , mc.account_type
+     , usd.guid as commodity_guid
+     , usd.fraction as commodity_scu
+     , 0 as non_std_scu
+     , parent.guid as parent_guid
+     , '' as code
+     , '' as description
+     , 0 as hidden
+     , 0 as placeholder
+from (select category, case when tot > 0 then 'INCOME' else 'EXPENSE' end as account_type
+   from (
+     select category
+          , sum(amount * case when transaction_type = 'debit' then -1 else 1 end) as tot
+     from mintexport
+     group by category) x) mc
+join commodities usd on usd.mnemonic='USD'
+join accounts parent on parent.name=
+   case when mc.account_type = 'EXPENSE' then 'Mint Expenses' else 'Mint Income' end
+order by 3, 2;
+-- 124 row(s) affected
+
+select distinct slot_type from slots
+order by 1;
+select * from slots where slot_type=10;
+-- 1: int64_val
+-- 4: string_val
+-- 5: guid_val
+-- 6: timespec_val
+-- 9: guid_val again?
+-- 10: gdate_val
+
+select acct.name, s.*
+from accounts acct
+join slots s on s.obj_guid = acct.guid
+order by 1;
+
+insert into slots (obj_guid, name, slot_type, string_val)
+select acct.guid obj_guid
+     , 'color' name
+     , 4 slot_type
+     , 'Not Set' string_val
+from accounts parent
+join accounts acct on acct.parent_guid = parent.guid
+where parent.name='Mint Export';
+
+
+-- create index trx_sig on mintexport (date, original_description(300), transaction_type, amount);
 
 select count(*), date, original_description, transaction_type, amount
 from mintexport
