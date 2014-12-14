ef5740c3d64333fc887740523d1419aa7f0214d5
Dan Connolly <http://www.madmode.com/>
2014-12-14 14:00:37 -0600

    tidy_trx: misc updates

--- tidy_trx.sql
+++ tidy_trx.sql
@@ -1,6 +1,7 @@
 use dm93finance;
 
 SET sql_safe_updates=0;
+set autocommit=0;
 
 -- clean up BankBV transaction descriptions
 update transactions t
@@ -20,3 +21,24 @@ set t.description = fx.fixed
 where fx.fixed is not null
 ;
 
+-- XX6208 POS PURCHASE AT 09/30 04:
+-- XX6208 POS PURCHASE AT 09/30 04:54 JCPENNEY 2990 OVERLAND PARK KS 57795546 97164
+
+select * from split_detail
+where description like '%POS PURCHASE%';
+
+select * from split_detail
+where memo like '%POS PURCHASE%';
+
+select tx.post_date, tx.description, s.memo,
+  substr(s.memo, length('XX6208 POS PURCHASE AT 09/30 04:54 _'))
+from transactions tx
+join splits s on s.tx_guid = tx.guid
+where s.memo like '%POS PURCHASE AT%';
+
+update transactions tx
+ join splits s on s.tx_guid = tx.guid
+set tx.description = substr(s.memo, length('XX6208 POS PURCHASE AT 09/30 04:54 _'))
+where tx.description like '%POS PURCHASE AT%';
+
+commit;
